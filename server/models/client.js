c63912662f1832d33e824dcb7280035fd10a8e59
Ali Hussam <38767751+alihussam@users.noreply.github.com>
2024-07-24 12:33:41 +0500

    complete project

---
+++ server/models/client.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const ClientSchema = new mongoose.Schema({
+  name: {
+    type: String,
+  },
+  email: {
+    type: String,
+  },
+  phone: {
+    type: String,
+  },
+});
+
+const ClientModel = mongoose.model("Client", ClientSchema);
+module.exports = ClientModel;
---
+++ server/models/project.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const ProjectSchema = new mongoose.Schema({
+  name: {
+    type: String,
+  },
+  description: {
+    type: String,
+  },
+  status: {
+    type: String,
+    enum: ["Not Started", "In Progress", "Completed"],
+  },
+  clientId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Client",
+  },
+});
+
+const ProjectModel = mongoose.model("Project", ProjectSchema);
+module.exports = ProjectModel;
